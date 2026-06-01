d175b9f87a6dedbbee76ddc2f9b04b316b80df99
gt4dm229kr-rgb <gt4dm229kr@privaterelay.appleid.com>
2026-06-01 16:24:01 +0200

    Risolto errore nomi componenti

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
 import Hero from '@/components/Hero';
 import Services from '@/components/Services';
-import RetinaFocus from '@/components/RetinaFocus';
+import Hero from '@/components/Hero';
 import About from '@/components/About';
-import Research from '@/components/Research';
-import FAQ from '@/components/FAQ';
+import Services from '@/components/Services';
 import Contact from '@/components/Contact';
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <Services />
-      <RetinaFocus />
-      <About />
-      <Research />
-      <FAQ />
-      <Contact />
-    </>
-  );
+return (
+  <main>
+    <Hero />
+    <About />
+    <Services />
+    <Contact />
+  </main>
+)
 }
